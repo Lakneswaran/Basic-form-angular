4cb0d51cad232de43843f0d663ad17a4f26bfe9b
Lakneswaran <90680982+Lakneswaran@users.noreply.github.com>
2021-12-01 12:02:51 +0100

    step 15 is done, imported friend, post request done

--- basic-form-angular-/src/app/add-friend.service.ts
+++ basic-form-angular-/src/app/add-friend.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Friend } from './friend';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +9,13 @@ export class AddFriendService {
 
   constructor(private http:HttpClient) {
 
+   }
+
+   
+   var url = "";
+   addFriend(friend:Friend){
+
+    this.http.post(this.url, { title: 'Angular POST Request Example' }).subscribe(data => {
+     })
    }
 }
